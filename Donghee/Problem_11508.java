3952d22b9b677da0b72986d9a235ff13044a8036
이 동희 <ehdgml7732@naver.com>
2021-02-02 16:58:06 +0900

    baekjoon_11508_2+1
    Using BufferedReader
    988ms->380ms.

--- Donghee/Problem_11508.java
+++ Donghee/Problem_11508.java
@@ -1,21 +1,22 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
-/* 2+1 문제, ArraySort할 때 내림차순 하는 방법?? */
+/* 2+1 문제, ArraySort할 때 내림차순 하는 방법??  */
 public class Problem_11508 {
 	private static int N;
 	private static int []map;
 	private static int free, sum;
 	
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		N=sc.nextInt();
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		N=Integer.parseInt(br.readLine());
 		map=new int[N];
-
 		for(int i=0; i<N; i++) {
-			map[i]=sc.nextInt();
+			map[i]=Integer.parseInt(br.readLine());
 			sum+=map[i];
 		}
 		
