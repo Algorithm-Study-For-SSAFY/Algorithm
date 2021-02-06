d3c2dce0696f33756ebc5262b056ed7e6f8a7302
kwanheumcho <rhksgma258@gmail.com>
2021-02-06 14:57:58 +0900

    2주차_파도반수열

---
+++ Kwanheum/0203-0209/boj9461.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class boj9461 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		long [] P = new long[101]; // 0 => dummy
+		P[1] = 1;
+		P[2] = 1;
+		P[3] = 1;
+		P[4] = 2;
+		P[5] = 2;
+		for(int i=6; i<=100; i++) {
+			P[i] = P[i-1] + P[i-5];
+		}
+		for(int t=0; t<T; t++) {
+			int input = sc.nextInt();
+			System.out.println(P[input]);
+		}
+	}//main
+}
--- Kwanheum/0203-0209/test.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-import java.util.ArrayList;
-
-public class test {
-	public static void main(String[] args) throws IOException {
-		
-	}
-}
