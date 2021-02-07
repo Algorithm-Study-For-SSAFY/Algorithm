f438659a79ee5c1ebeabc512dac808a8aacbdf19
kwanheumcho <rhksgma258@gmail.com>
2021-02-07 14:45:52 +0900

    2주차_팰린드롬 만들기 update

--- Kwanheum/0203-0209/boj1254.java
+++ Kwanheum/0203-0209/boj1254.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class boj1254 {
 	public static void main(String[] args) {
@@ -6,7 +7,9 @@ public class boj1254 {
 		Scanner sc = new Scanner(System.in);
 		String x = sc.next();
 		int len = x.length();
-		if(len % 2 ==0) {
+
+		/* Solution1. index로 처리해보기
+		 * if(len % 2 ==0) {
 			for(int i=len/2; i<len; i++) {
 				//중간선 기준 대칭 bbaa
 				for(int j=0; j<len-i; j++) {
@@ -52,6 +55,73 @@ public class boj1254 {
 				
 			}
 			
+		}*/
+		/////////////////////////////////////////////////
+		// Solution2. isPal 함수 활용하기
+		/////////////////////////////////////////////////
+		
+		if(len % 2 == 0) {
+			//bbaa
+			//중간선 기준 대칭 먼저 , 중간값 기준 대칭
+			for(int i=len/2; i<len; i++) {
+				// 중간선 기준.
+				String left = x.substring(i - (len-i), i);
+				String right = x.substring(i);
+				if(isPal(left,right)) {
+					System.out.println(len + len-2*(left.length()));
+					return;
+				}
+				// 중간값 기준.
+				//len-1 -i
+				int gap = len-1-i;
+				left = x.substring(i-gap);
+				if(isPal(left, left)) {
+					System.out.println(len + len-(left.length()));
+					return;
+				}
+			}
 		}
+		else {
+			//bbaaa
+			//중간값 기준 대칭 먼저, 중간선 기준 대칭
+			for(int i=len/2; i<len; i++) {
+				String left;
+				String right;
+				//중간선 기준. i=len/2일때 한번은 생략하게 조건문 설정
+				if(i > len/2) {
+					left = x.substring(i - (len-i), i);
+					right = x.substring(i);
+					if(isPal(left,right)) {
+						System.out.println(len + len-2*(left.length()));
+						return;
+					}
+				}
+				// 중간값 기준.
+				//len-1 -i
+				int gap = len-1-i;
+				left = x.substring(i-gap);
+				if(isPal(left, left)) {
+					System.out.println(len + len-(left.length()));
+					return;
+				}
+				
+			}
+		}
+		
+		
+	} // main
+	static boolean isPal(String a, String b) {
+		//b 문자열을 stack에 넣어서, a와 비교
+		Stack<Character> s = new Stack<>();
+		for(int i=0; i<a.length(); i++) {
+			s.push(b.charAt(i));
+		}
+		
+		for(int i=0; i<a.length(); i++) {
+			if(a.charAt(i) != s.pop()) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
