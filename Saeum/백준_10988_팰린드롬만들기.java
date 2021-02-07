463e4f7eb575a99f8a7ef208c8297c5eef49fd2e
saeumi <78096339+saeumi@users.noreply.github.com>
2021-02-08 03:40:46 +0900

    Add files via upload

---
+++ Saeum/백준_10988_팰린드롬만들기.java
@@ -0,0 +1,35 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 백준_10988_팰린드롬만들기 {
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		System.out.println(solution(input));
+	}
+
+	static int solution(String input) {
+
+		int len = input.length();
+		for (int i = 0; i < len; i++) {
+			if (isPalindrome(input.substring(i))) {
+				return len + i;
+			}
+		}
+		return len;
+	}
+
+	static boolean isPalindrome(String input) {
+
+		int len = input.length();
+		for (int i = 0; i < len / 2; i++) {
+			if (input.charAt(i) != input.charAt(len - i - 1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
