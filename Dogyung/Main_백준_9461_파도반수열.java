d0a60533bde650d8a3586d997bd8c3f238a61f3a
dokyeong10 <78183382+dokyeong10@users.noreply.github.com>
2021-02-09 13:46:29 +0900

    Add files via upload

---
+++ Dogyung/Main_백준_9461_파도반수열.java
@@ -0,0 +1,23 @@
+package al_2주차;
+import java.util.Scanner;
+public class Main_백준_9461_파도반수열 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T =sc.nextInt();
+		long arr[] = new long [101];
+		arr[1]=1;
+		arr[2]=1;
+		arr[3]=1;
+		arr[4]=2;
+		arr[5]=2;
+		
+		for (int i = 6; i <=100; i++) {
+			arr[i]=arr[i-1]+arr[i-5];
+		}
+		for (int i = 1; i <=T; i++) {
+			int num =sc.nextInt();
+			System.out.println(arr[num]);
+		}
+		
+	} // end of main
+}// end of class
