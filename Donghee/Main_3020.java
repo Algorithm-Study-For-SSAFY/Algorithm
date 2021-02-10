60cbd41fe597c339d56cc558d2c7d8ee18bfb005
이 동희 <ehdgml7732@naver.com>
2021-02-10 13:19:19 +0900

    baekjoon_3020_개똥벌레 update

--- Donghee/Main_3020.java
+++ Donghee/Main_3020.java
@@ -7,7 +7,6 @@ import java.util.StringTokenizer;
 
 /*
  * 개똥벌레, 파괴해야할 장애물의 최솟값과 그러한 구간의 수.
- * 시간초과가 발생할 줄 알았는데 안나는 이유,,,,,,,.....N,H 범위 참고..
  */
 public class Main_3020 {
 	public static void main(String[] args) throws IOException {
@@ -15,38 +14,45 @@ public class Main_3020 {
 		StringTokenizer st=new StringTokenizer(br.readLine());
 		int N=Integer.parseInt(st.nextToken());
 		int H=Integer.parseInt(st.nextToken());
-		int []cnt= new int[N+1];
+		int []map1=new int[H+1];
+		int []map2=new int[H+1];
 		
 		for(int i=1; i<=N; i++) {
 			int a=Integer.parseInt(br.readLine());
 			if(i%2==1) { //석순이라면, cnt를 ++
-				for(int j=1; j<=a; j++) {
-					cnt[j]++;
-				}
+				map1[a]++;
 			}else { //종유석이라면, 
-				for(int j=H; j>H-a; j--) {
-					cnt[j]++;
-				}
+				map2[H-a+1]++;
 			}
-		} //높이 입력받기, 시간 초과 발생?
-		int answer=Integer.MAX_VALUE;
-		int count=0; //answer와 같은 값 count로 계산하기
-		//높이 1일 때 부터 살펴보기,
-		for(int h=1; h<=H; h++) {
-			if(answer>cnt[h]) { //더 작은 값이 answer로 들어온다면,
-				count=1;
-				answer=cnt[h]; //answer 값 갱신
-			}else if(answer==cnt[h]) count++;
-			
-			
-			//높이가 h일 때 , cnt[h]의 갯수가 answer
-			answer=Math.min(answer, cnt[h]);
-			
-			
+		}//end of input
+		
+		//입력받은 높이에 따라, 값 주기
+		//석순인 경우, 위->아래
+		for(int i=H; i>=2; i--) {
+			map1[i-1]+=map1[i];
 		}
-		System.out.println(answer + " "+ count);
+		
+		//종유석인경우 아래->위
+		for(int i=2; i<=H; i++) {
+			map2[i]+=map2[i-1];
+		}
+		//최솟값 찾기
+		int answer=Integer.MAX_VALUE;
+		int count=0;
+		for(int h=1; h<=H; h++) {
+			if(answer==map1[h]+map2[h]) {
+				count++;
+			}else if(answer>=(map1[h]+map2[h])) {
+				count=1;
+				answer=map1[h]+map2[h];
+			}
+		}
+		System.out.println(answer+" "+count);
 	}//end of main
 }
 
+
 // 저는 메모리 초과를 해결했더니 시간 초과가 발생해서 결국 반복하다가 포기했어요ㅠㅠㅠ
 // 어떻게 해야 해결할 수 있는 걸까요,
+
+// 저도 다시 해보니 시간초과가 나왔다고 떠서 다시 제출했습니다.. ㅠ 
