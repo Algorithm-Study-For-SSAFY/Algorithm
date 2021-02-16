8b6462a0413dd8d75dcb06113dd82d81687cece8
Donghee Lee <ehdgml7732@naver.com>
2021-02-17 01:14:31 +0900

    baekjoon_가장 가까운 조상

--- Donghee/0211-0217/Main_3584.java
+++ Donghee/0211-0217/Main_3584.java
@@ -6,67 +6,65 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main_3584 {
+public class Main_가장가까운조상 {
 	static int T, N;
-	static ArrayList<Integer> tree[];
-	static ArrayList<Integer> ad1; //주어진 하나의 노드의 부모, 조상노드를 모아놀 배열
+	
+	static ArrayList<Integer> ad1; //구하려는 node1, node2 각각의 조상들을 모아놓은 arraylist.
 	static ArrayList<Integer> ad2;
+	
+	static int tree[];
 	static int node1, node2;
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int T=Integer.parseInt(br.readLine());
 		StringTokenizer st;
 		for(int tc=1; tc<=T; tc++) {
 			N=Integer.parseInt(br.readLine());
-			tree=new ArrayList[N+1]; //트리 1번~N번,
-			for(int i=0; i<=N; i++) {
-				tree[i]=new ArrayList<>();
-			}
+			tree=new int[N+1]; //트리 1번~N번,
+			tree[0]=-1; //노드 0번은 없으니까
+			
 			for(int i=0; i<N-1; i++) {
 				st=new StringTokenizer(br.readLine());
 				int a=Integer.parseInt(st.nextToken());
 				int b=Integer.parseInt(st.nextToken()); //a가 b의 부모다.
-				//자식의 tree 배열에 부모 노드 집어넣기
-				tree[b].add(a);
-			}//간선정보 입력받기
-
+				tree[b]=a; //자식노드 idx에 부모노드 값 집어넣기.
+			}//루트 노드 : 값이 0인 노드,기본값 0
+			
 			st=new StringTokenizer(br.readLine());
 			node1=Integer.parseInt(st.nextToken());
 			node2=Integer.parseInt(st.nextToken()); //조상이 궁금한 노드 1, 2
-			ad1=new ArrayList<>();
-			ad2=new ArrayList<>();
+			
+			//자기 자신이 조상이 될 수 있으므로 노드 집어넣기
+			ad1=new ArrayList<Integer>();
+			ad2=new ArrayList<Integer>();
 			//end of input
 			ad1.add(node1); //자기 자신 노드 넣기
 			ad2.add(node2); //자기 자신 노드 넣기
-			searchParent(1, node1, ad1);
-			searchParent(1,node2, ad2);
-			//ad1과 ad2가 같은게 나올 때 까지 진행 루트 노드부터보자.
-//			for(int i=0; i<ad1.size(); i++)
-//				System.out.println(ad1.get(i));
-//			System.out.println("****");
-//			for(int i=0; i<ad2.size(); i++)
-//				System.out.println(ad2.get(i));
 			
+			searchParent(node1, ad1);
+			searchParent(node2, ad2);
+			
+			//ad1과 ad2의 마지막 원소는 무조건 루트노드,
 			int i=ad1.size()-1; int j=ad2.size()-1;
-			System.out.println(i+" "+j);
-			//처음엔 root노드라서 무조건 같음. 
-			while(ad1.get(i)==ad2.get(j)) {
+			
+			while(ad1.get(i).equals(ad2.get(j))) { //같을 때 동안 반복
 				i--; j--;
 				if(i<0 || j<0)
 					break;
 			}
-			if(i<0) System.out.println(ad1.get(0));
-			else if(j<0) System.out.println(ad2.get(0));
-			else System.out.println(ad1.get(i+1));
 			
+			if(i<0) System.out.println(node1); //적어도 ad1의 0과 ad2의 어떤것은 같은 노드였다는 것.
+			else if(j<0) System.out.println(node2);
+			else System.out.println(ad1.get(i+1));
 		}//end of tc
 	}//end of main
-	//arraylist에 연속적인 부모노드 집어넣기,
-	//ex. 1번노드의 부모 2번, 2번노드의 부모노드 3번,,,,,
-	private static void searchParent(int idx, int node, ArrayList<Integer> arr) {
-		if(tree[node].isEmpty()) 
-			return; //루트 노드면 종료
-		arr.add(tree[node].get(0)); //node의 부모노드 넣는 작업
-		searchParent(idx+1, arr.get(idx), arr);
+	
+	private static void searchParent(int node, ArrayList<Integer> list) {
+		
+		//전달받은 node의 부모로 올라가기
+		if(tree[node]==0)
+			return; //얘가 루트노드란 뜻 (부모가 없으니까)
+		list.add(tree[node]);
+		searchParent(tree[node], list);
 	}
-}//end of class
+}
