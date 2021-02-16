f99abd471e8fc6155ae692c2c947f6b1768108c4
Saeeum Park <78096339+saeumi@users.noreply.github.com>
2021-02-16 22:47:11 +0900

    Add files via upload

---
+++ Saeum/3주차/백준_1946_신입사원.java
@@ -0,0 +1,35 @@
+package week3;
+
+import java.util.*;
+
+public class 백준_1946_신입사원 {
+
+	//순위는 면접이랑 서류 모두 1~N이니까 
+	//면접 서류 중 하나를 index로 설정 
+	
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); 
+
+        for (int i = 0; i < t; i++) {
+            int loop = sc.nextInt();
+            int[] candidates = new int[loop + 1];
+            for (int j = 0; j < loop; j++) {
+                candidates[sc.nextInt()] = sc.nextInt();
+            }
+
+            int count = 1;
+            int interviewRank = candidates[1];
+
+            for (int k = 2; k <= loop; k++) {
+                if (interviewRank >= candidates[k]) {
+                    interviewRank = candidates[k];
+                    count++;
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
+}
