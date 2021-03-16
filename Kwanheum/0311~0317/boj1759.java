60f225d59daf9fd39480d53c1492e4378ddbffb5
kwanheumcho <rhksgma258@gmail.com>
2021-03-16 17:04:32 +0900

    7주차_암호만들기

---
+++ Kwanheum/0311~0317/boj1759.java
@@ -0,0 +1,54 @@
+package algo_study_7w;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class boj1759 {
+	private static int L;
+	private static int C;
+	private static char[] arr;
+	private static List<String> answer;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+		L = Integer.parseInt(stk.nextToken());
+		C  = Integer.parseInt(stk.nextToken());
+		stk = new StringTokenizer(br.readLine());
+		arr = new char[C];
+		for(int i=0; i<C; i++) {
+			arr[i] = stk.nextToken().charAt(0);
+		}
+		Arrays.sort(arr);
+		answer = new LinkedList<>();
+		func(0, 0, 0, "");
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<answer.size();i++) {
+			sb.append(answer.get(i)).append("\n");
+		}
+		System.out.println(sb);
+	}//main
+	
+	static void func(int cnt, int m, int j, String pw) {
+		if( m+j == L) {
+			if(m>=1 && j>=2) {
+				answer.add(pw);
+			}
+			return;
+		}
+		
+		for(int i=cnt; i<C; i++) {
+			char x = arr[i];
+			if( "aeiou".contains(x+"")) {
+				func(i+1, m+1, j, pw+x);
+			}
+			else {
+				func(i+1, m, j+1, pw+x);
+			}
+		}
+	}
+}
